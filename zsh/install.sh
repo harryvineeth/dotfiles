10e30ad9310d337b84b2a7e4a55de16233854c0d
NellyWhadsDev <nellywhads@gmail.com>
2018-06-08 12:22:46 -0400

    Need to update before install for anything but ubuntu 18.04

--- zsh/install.sh
+++ zsh/install.sh
@@ -11,6 +11,7 @@ curl -L git.io/antigen > $ANTIGEN_DIR/antigen.zsh
 
 printf '%s\n' "Installing Dependancy: fzy..." 1>&2
 curl -s https://packagecloud.io/install/repositories/jhawthorn/fzy/script.deb.sh | sudo bash
+sudo apt-get update
 sudo apt-get install fzy -y
 
 printf '%s\n' "Installing ZSH..." 1>&2

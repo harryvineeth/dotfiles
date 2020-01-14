d9080fa04dba7e58a3ab8873ad8a15424dc41ec2
Nelly Whads <nellywhads@gmail.com>
2020-01-14 11:14:46 -0800

    Disable VSCode Insiders on macOS

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # dotfiles
 My dotfiles for a clean OS rampup.
 
-### Supported OSes (detected automatically)
+## Supported OSes (detected automatically)
+
 - Ubuntu (Tested on >= 16.04)
 - macOS (Tested on >= 10.11)
 
+## Usage
+
+`./install.sh`
+
 ### Headless mode
+
+`./install.sh --headless`
+
 This installs only headless tools/applications and omits things such as Google Chrome and VSCode.
+
+### TODO
+
+- Re-add macOS Visual Studio Code Insiders (cask is no longer available)
--- vscode/install.sh
+++ vscode/install.sh
@@ -3,8 +3,8 @@ printf '\n\e[34;1m%s\e[0m\n\n' "--------VSCode Installation--------" 1>&2
 printf '\e[34m%s\e[0m\n' "Installing Visual Studio Code Insiders..." 1>&2
 if [ "$MACHINE" = "Ubuntu" ] && [ "$UI_TYPE" != "headless" ]; then
     sudo snap install --classic code-insiders
-elif [ "$MACHINE" = "MacOS" ] && [ "$UI_TYPE" != "headless" ]; then
-    brew cask install visual-studio-code-insiders
+# elif [ "$MACHINE" = "MacOS" ] && [ "$UI_TYPE" != "headless" ]; then
+#     brew cask install visual-studio-code-insiders
 else
     printf '\e[31m%s\e[0m\n' "Visual Studio Code Insiders not installed..." 1>&2
 fi
